cf4ec60f5b433cbcf3776e9c4ff0221e2a57b809
Sebastian Widin <contact@sebastianwidin.se>
2023-11-02 00:11:35 +0100

    change form action

--- src/components/MailPopup.tsx
+++ src/components/MailPopup.tsx
@@ -1,27 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import { toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
 
 import styles from '@/styles/Popup.module.scss'
 import Button from './Button'
 
 const MailPopup = ({ newsletter }: any) => {
 	const [open, setOpen] = useState(false)
-	const [submitting, setSubmitting] = useState(false)
-	const [email, setEmail] = useState('')
-
-	const notification = (message: string) => {
-		if (message === 'success') {
-			toast.success('Tack för att du gått med i vårt nyhetsbrev!', {
-				position: toast.POSITION.BOTTOM_CENTER,
-			})
-		} else if (message === 'fail') {
-			toast.error('Åh nej! Något gick fel, var vänlig försök igen.', {
-				position: toast.POSITION.BOTTOM_CENTER,
-			})
-		}
-	}
 
 	useEffect(() => {
 		const newsLetterShown = localStorage.getItem('newsLetterShown')
@@ -43,38 +26,11 @@ const MailPopup = ({ newsletter }: any) => {
 		setOpen(false)
 	}
 
-	const submitMailForm = (e: React.FormEvent) => {
-		e.preventDefault()
-		setSubmitting(true)
-
-		axios
-			.post(
-				'https://Dalafisk.us9.list-manage.com/subscribe/post?u=ccd90fd17dce57c1ad1f1f663&id=ef7ad01277&f_id=004722e1f0',
-				{ MAIL: email, b_ccd90fd17dce57c1ad1f1f663_ef7ad01277: '' },
-				{
-					headers: {
-						'Access-Control-Allow-Origin': '*',
-					},
-				}
-			)
-			.then((res) => {
-				if (res.status === 200) {
-					notification('success')
-					localStorage.setItem('newsLetterShown', 'signed-up')
-					setOpen(false)
-				} else {
-					setSubmitting(false)
-					notification('fail')
-					console.error('Not submitted')
-					console.error(e)
-				}
-			})
-			.catch((e) => {
-				setSubmitting(false)
-				notification('fail')
-				console.error('Not submitted')
-				console.error(e)
-			})
+	const submittedMailForm = () => {
+		setTimeout(() => {
+			localStorage.setItem('newsLetterShown', 'signed-up')
+			setOpen(false)
+		}, 200)
 	}
 
 	if (open) {
@@ -89,21 +45,35 @@ const MailPopup = ({ newsletter }: any) => {
 							__html: newsletter.body,
 						}}
 					/>
-					<form onSubmit={(e) => submitMailForm(e)} className={styles.form}>
+					<form
+						action='https://Dalafisk.us9.list-manage.com/subscribe/post?u=ccd90fd17dce57c1ad1f1f663&id=ef7ad01277&f_id=004722e1f0'
+						method='post'
+						id='mc-embedded-subscribe-form'
+						name='mc-embedded-subscribe-form'
+						target='_blank'
+						className={styles.form}
+					>
 						<div className={styles.input_container}>
-							<label htmlFor='email'>Din e-post</label>
+							<label htmlFor='mce-EMAIL'>Din e-post</label>
 							<input
 								type='email'
-								name='email'
-								id='email'
-								placeholder='Din e-postadress'
-								value={email}
-								onChange={(e) => setEmail(e.target.value)}
+								name='EMAIL'
+								id='mce-EMAIL'
 								required
+								placeholder='Din e-postadress'
+							/>
+							<input
+								className={styles.hidden}
+								type='text'
+								name='b_ccd90fd17dce57c1ad1f1f663_ef7ad01277'
 							/>
 						</div>
 						<div className={styles.button_container}>
-							<Button primary type='submit' disabled={submitting}>
+							<Button
+								primary
+								clickCallback={() => submittedMailForm()}
+								type='submit'
+							>
 								Ja tack!
 							</Button>
 							<Button clickCallback={() => closePopup()}>Nej tack...</Button>
